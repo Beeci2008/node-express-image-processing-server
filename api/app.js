cf951dd485228742615080b76dc245c08d64c36e
Rebecca Hein <rebeccahein@Rebeccas-MacBook-Pro.local>
2022-08-28 16:54:25 -0700

    module 3

--- api/app.js
+++ api/app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const path = require('path')
+const router = require('./src/router');
 
 const app = express();
+
+app.use('/', router)
 
 const pathToIndex = path.resolve(__dirname, '../client/index.html')
 app.use('/*', function (request, response) {
     response.sendFile(pathToIndex);
 })
+
+app.use(express.static(path.resolve(__dirname, 'uploads')))
+
 module.exports = app;
